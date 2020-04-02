9bd0d012da8d1d2adbbba64258246333be4d9f6e
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2020-04-02 11:32:03 +0200

    Try to migrate all tables

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -8,11 +8,15 @@ $wc.DownloadFile($url, $output)
 $unzipTo = "$PSScriptRoot\unzipped\"
 Expand-Archive -LiteralPath $output -DestinationPath $unzipTo
 
-Write-Output "Execute"
 $Command = "$unzipTo\drop\dt.exe"
-$Parms = "/s:AzureTable /s.ConnectionString:" + $env:BLOB_STORAGE_CONNECTION + " /s.Table:Roles  /t:TableAPIBulk /t.ConnectionString:" + $env:COSMOSDB_CONNECTION + " /t.TableName:Roles"
 
-$Prms = $Parms.Split(" ")
-& "$Command" $Prms
+$tables = @("Roles", "Experiences", "QAComments", "Resources", "RolesSnapshot")
+foreach ($table in $tables) {
+    Write-Output "Migrating " + $table
+    $Parms = "/s:AzureTable /s.ConnectionString:" + $env:BLOB_STORAGE_CONNECTION + " /s.Table:" + $table + "  /t:TableAPIBulk /t.ConnectionString:" + $env:COSMOSDB_CONNECTION + " /t.TableName:Roles"
 
-Write-Output "IKNU TEST: Done executing"
+    $Prms = $Parms.Split(" ")
+    & "$Command" $Prms
+}
+
+Write-Output "All tables migrated"
