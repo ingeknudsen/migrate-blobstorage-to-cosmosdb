81490ae798d6dca336095b7aafeafab30358bc0f
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2020-04-02 11:07:35 +0200

    Echo params to see that the string looks ok

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -11,6 +11,7 @@ Expand-Archive -LiteralPath $output -DestinationPath $unzipTo
 Write-Output "Execute"
 $Command = "$unzipTo\drop\dt.exe"
 $Parms = "/s:AzureTable /s.ConnectionString:" + $env:BLOB_STORAGE_CONNECTION + " /s.Table:Roles /s.InternalFields:All /s.Projection:ObjectCount; ObjectSize  /t:DocumentDBBulk /t.ConnectionString:" + $env:COSMOSDB_CONNECTION + " /t.Collection:Roles /t.CollectionThroughput:2500"
+Write-Output $Parms
 
 $Prms = $Parms.Split(" ")
 & "$Command" $Prms
