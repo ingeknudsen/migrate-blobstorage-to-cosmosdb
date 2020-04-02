26671b48ed449e06c86e2885b1ae1aa8acf729b5
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2020-04-02 11:14:38 +0200

    Try a new connection string

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -11,7 +11,6 @@ Expand-Archive -LiteralPath $output -DestinationPath $unzipTo
 Write-Output "Execute"
 $Command = "$unzipTo\drop\dt.exe"
 $Parms = "/s:AzureTable /s.ConnectionString:" + $env:BLOB_STORAGE_CONNECTION + " /s.Table:Roles /s.InternalFields:All /s.Projection:ObjectCount; ObjectSize  /t:DocumentDBBulk /t.ConnectionString:" + $env:COSMOSDB_CONNECTION + " /t.Collection:Roles /t.CollectionThroughput:2500"
-Write-Output $Parms
 
 $Prms = $Parms.Split(" ")
 & "$Command" $Prms
